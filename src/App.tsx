cf2a6c86c6ecbea6e150eb87ea0835aebdf6b776
Elix Neto <neto.elix@outlook.com>
2023-09-30 18:38:29 -0300

    Finalizando o requisito 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './styles/App.css';
+import { Route, Routes } from 'react-router-dom';
 import { About, NotFound, Posts, Users } from './pages';
 
 function App() {
-  return (<p>Exercício Bloggers - React Router</p>);
+  return (
+    <Routes>
+      <Route path="/" element={ <Users /> } />
+      <Route path="/posts/:id" element={ <Posts /> } />
+      <Route path="/about" element={ <About /> } />
+      <Route path="/*" element={ <NotFound /> } />
+    </Routes>
+  );
 }
 
 export default App;
