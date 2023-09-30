f9373de211e0c2b9bb741e5bda255d92e80ab42d
Elix Neto <neto.elix@outlook.com>
2023-09-30 18:48:06 -0300

    Finalizando requisito 3

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -2,6 +2,7 @@ import Header from '../components/Header';
 import User from '../components/User';
 import { users } from '../data';
 import '../styles/Users.css';
+import { Link } from 'react-router-dom';
 
 export default function Users() {
   return (
@@ -10,7 +11,12 @@ export default function Users() {
       <h1>Users</h1>
       <div className="users-list">
         {users.map((user) => (
-          <User key={ user.id } userData={ user } />
+          <>
+            <User key={ user.id } userData={ user } />
+            <Link to={ `/posts/${user.id}` }>
+              Posts
+            </Link>
+          </>
         ))}
       </div>
     </div>
